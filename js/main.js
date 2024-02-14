b564be353ab847db9f538556243f90828a28304f
JuanPabloCardenasGonzalez <jpcardenasgonzalez@gmail.com>
2024-02-14 17:13:29 -0500

    fix: :bug: Mostrar los vueltos de la compra
    Se corrigió que al mostrar los vueltos de la comprar no se mostraran los vueltos de la compra pasada
    
    @CampusLands

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const modal = document.querySelector("#modal");
+const modalContent = document.querySelector(".modal-content");
 
-function cerrarModal () {
+function cerrarModal() {
   modal.style.display = "none";
-};
+  const pContainer = document.getElementById("pContainer");
+  if (pContainer) {
+    pContainer.remove();
+  }
+}
 
 function comprar(precio) {
   let cantidadDinero = 0;
@@ -18,25 +23,26 @@ function comprar(precio) {
     }
   }
 
-  const modal = document.querySelector("#modal");
-  const modalContent = document.querySelector(".modal-content");
+  const pContainer = document.createElement("div");
+  pContainer.id = "pContainer";
 
-  modalContent.innerHTML += `Su vuelto es: <br/>`;
+  modalContent.appendChild(pContainer);
+  pContainer.innerHTML += "<p>Su vuelto es:</p> <br/>";
   if (vuelto === 0) {
     modal.style.display = "block";
-    modalContent.innerHTML += "0 pesos";
+    pContainer.innerHTML += "<p>0 pesos</p>";
   } else {
     modal.style.display = "block";
     while (Math.floor(vuelto / 100) > 0) {
-      modalContent.innerHTML += "100 <br/>";
+      pContainer.innerHTML += "<p>$ 100</p><br/>";
       vuelto -= 100;
     }
     while (Math.floor(vuelto / 50) > 0) {
-      modalContent.innerHTML += "50 <br/>";
+      pContainer.innerHTML += "<p>$ 50</p><br/>";
       vuelto -= 50;
     }
     while (Math.floor(vuelto / 10) > 0) {
-      modalContent.innerHTML += "10 <br/>";
+      pContainer.innerHTML += "<p>$ 10</p><br/>";
       vuelto -= 10;
     }
   }
